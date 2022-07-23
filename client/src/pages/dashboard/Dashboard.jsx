b39f204bea70fdf151252dc1927c94fdf1a6629a
mikhail-svekolnikov <m.svekolnikov@itransition.com>
2022-07-23 20:51:30 +0300

    Frontend - dashboard cards

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -2,12 +2,96 @@ import React from "react";
 import { Layout } from "../../components/layout/Layout";
 import { Card } from "../../components/card/Card";
 import { PieChart } from "../../components/pieChart/PieChart";
+import { Col, Row, Statistic } from "antd";
+import styled from "styled-components";
+import { AiOutlineArrowDown, AiOutlineArrowUp } from "react-icons/all";
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 20px 40px;
+  background-color: #1890ff;
+  border-radius: 10px;
+  color: white;
+
+  .ant-statistic-title {
+    color: ${({ isGrow }) => (isGrow ? "rgb(142, 255, 86)" : "darkred")};
+  }
+`;
+
+const getValueForStatistic = () => {
+  return Math.floor(Math.random() * (100 + 1));
+};
+
+const cards = [
+  {
+    name: "Customers",
+    value: 1220,
+    diff: getValueForStatistic(),
+  },
+  {
+    name: "Products",
+    value: 345,
+    diff: getValueForStatistic(),
+  },
+  {
+    name: "Sales",
+    value: 234,
+    diff: getValueForStatistic(),
+  },
+  {
+    name: "Benefits",
+    value: 2,
+    diff: getValueForStatistic(),
+  },
+  {
+    name: "Benefits",
+    value: 790,
+    diff: getValueForStatistic(),
+  },
+];
 
 export const Dashboard = () => {
   return (
     <Layout title="Dashboard">
       <Card>
-        <PieChart />
+        <Row style={{ display: "flex" }}>
+          <Col span={4}>
+            <PieChart />
+          </Col>
+          <Col
+            span={20}
+            style={{ display: "flex", justifyContent: "space-between" }}
+          >
+            {cards.map((card) => (
+              <ItemContainer isGrow={card.diff > 50}>
+                <div>
+                  <div style={{ fontWeight: "600", fontSize: 18 }}>
+                    {card.name}
+                  </div>
+                  <div style={{ fontWeight: 600, fontSize: 40 }}>
+                    {card.value}
+                  </div>
+                </div>
+                <Statistic
+                  title={card.diff > 50 ? "Grow" : "Down"}
+                  value={card.diff}
+                  precision={2}
+                  valueStyle={{ color: card.diff > 50 ? "#8eff56" : "darkred" }}
+                  prefix={
+                    card.diff > 50 ? (
+                      <AiOutlineArrowUp />
+                    ) : (
+                      <AiOutlineArrowDown />
+                    )
+                  }
+                  suffix="%"
+                />
+              </ItemContainer>
+            ))}
+          </Col>
+        </Row>
       </Card>
     </Layout>
   );
