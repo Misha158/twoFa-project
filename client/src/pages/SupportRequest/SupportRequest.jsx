b947dd4aec00be79a13b3689a5ff7fc6459de632
mihaosv <mihaosv@gmail.com>
2022-08-03 21:59:12 +0300

    Frontend - input phone test

--- client/src/pages/SupportRequest/SupportRequest.jsx
+++ client/src/pages/SupportRequest/SupportRequest.jsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import PhoneInputComponent from "react-phone-input-2";
+import "react-phone-input-2/lib/style.css";
 import { Card, Layout } from "../../components";
 
+import styled from "styled-components";
+
+export const PhoneInput = styled(PhoneInputComponent)`
+  /*  .selected-flag {
+    opacity: 0;
+  }*/
+`;
+
 export const SupportRequest = () => {
+  const [inputPhone, setInputPhone] = useState("");
+  console.log(inputPhone);
   return (
     <Layout title="SupportRequest">
-      <Card>SupportRequest</Card>
+      <Card>
+        <PhoneInput
+          country={"us"}
+          value={inputPhone}
+          onChange={(phone, country, _, formattedValue) => {
+            setInputPhone(phone);
+            console.log(country);
+            console.log(_);
+            console.log(formattedValue);
+          }}
+          placeholder="type your phone"
+        />
+      </Card>
     </Layout>
   );
 };
