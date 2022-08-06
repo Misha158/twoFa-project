5969bfacd7ab8e136d9d9cd448c27adc63a27110
mihaosv <mihaosv@gmail.com>
2022-08-06 15:15:55 +0300

    Frontend - slick slider

---
+++ client/src/components/Slider/Slider.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactSlider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
+export const Slider = () => {
+  return (
+    <div>
+      <h2> Single Item</h2>
+      <ReactSlider {...settings}>
+        <div>
+          <h3>1</h3>
+        </div>
+        <div>
+          <h3>2</h3>
+        </div>
+        <div>
+          <h3>3</h3>
+        </div>
+        <div>
+          <h3>4</h3>
+        </div>
+        <div>
+          <h3>5</h3>
+        </div>
+        <div>
+          <h3>6</h3>
+        </div>
+      </ReactSlider>
+    </div>
+  );
+};
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -3,3 +3,4 @@ export { Layout } from "./layout/Layout";
 export { Sidebar } from "./sidebar/Sidebar";
 export { PieChartGradient } from "./charts/pieChartGradient/PieChartGradient";
 export { PieChart } from "./charts/pieChart/PieChart";
+export { Slider } from "./Slider/Slider";
