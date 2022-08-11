069e7b32604e05b603421306b8611b7eface4922
mihaosv <mihaosv@gmail.com>
2022-08-11 23:21:19 +0300

    Algorithm- LinkedList implementation

---
+++ client/src/algorithms/LinkedList.js
@@ -0,0 +1,55 @@
+class LinkedListNode {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  toString() {
+    return `${this.value}`;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+    if (!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  toArray() {
+    const nodes = [];
+    let currentNode = this.head;
+
+    while (currentNode) {
+      nodes.push(currentNode);
+      currentNode = currentNode.next;
+    }
+
+    return nodes;
+  }
+
+  toString() {
+    return this.toArray()
+      .map((node) => node.toString())
+      .toString();
+  }
+}
+
+const List = new LinkedList().append("misha").append("tf2").append("dota2");
+// console.log(JSON.stringify(List));
+
+console.log(List.toString());
