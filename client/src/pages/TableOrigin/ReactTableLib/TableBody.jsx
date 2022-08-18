4570b216b8593c2967ab61922e5de819150a7ca5
mihaosv <mihaosv@gmail.com>
2022-08-18 19:55:53 +0300

    Frontend - react-table refactoring

---
+++ client/src/pages/TableOrigin/ReactTableLib/TableBody.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export const TableBody = ({ getTableBodyProps, rows, prepareRow }) => {
+  return (
+    <tbody {...getTableBodyProps()}>
+      {rows.map((row) => {
+        prepareRow(row);
+        return (
+          <tr {...row.getRowProps()}>
+            {row.cells.map((cell) => {
+              return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
+            })}
+          </tr>
+        );
+      })}
+    </tbody>
+  );
+};
---
+++ client/src/pages/TableOrigin/ReactTableLib/TableHead.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const TableHead = ({ headerGroups }) => {
+  return (
+    <thead>
+      {headerGroups.map((headerGroup) => (
+        <tr {...headerGroup.getHeaderGroupProps()}>
+          {headerGroup.headers.map((column) => (
+            <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+          ))}
+        </tr>
+      ))}
+    </thead>
+  );
+};
