26cdc9033028cce215fe105a360952430d678122
mihaosv <mihaosv@gmail.com>
2022-08-18 20:10:54 +0300

    Frontend - react-table refactoring + input

--- client/src/pages/TableOrigin/ReactTableLib/ReactTableLib.jsx
+++ client/src/pages/TableOrigin/ReactTableLib/ReactTableLib.jsx
@@ -1,78 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTable } from "react-table";
-import styled from "styled-components";
+import { StyledInput, Styles } from "./styled";
 import { TableHead } from "./TableHead";
+import { data } from "./data";
+import { columns } from "./columns";
 import { TableBody } from "./TableBody";
 
-const Styles = styled.div`
-  padding: 1rem;
-
-  table {
-    border-spacing: 0;
-    border: 1px solid black;
-
-    tr {
-      :last-child {
-        td {
-          border-bottom: 0;
-        }
-      }
-    }
-
-    th,
-    td {
-      margin: 0;
-      padding: 0.5rem;
-      border-bottom: 1px solid black;
-      border-right: 1px solid black;
-
-      :last-child {
-        border-right: 0;
-      }
-    }
-  }
-`;
-
 export const ReactTableLib = () => {
-  const data = React.useMemo(
-    () => [
-      {
-        col1: "Hello",
-        col2: "World",
-      },
-      {
-        col1: "react-table",
-        col2: "rocks",
-      },
-      {
-        col1: "whatever",
-        col2: "you want",
-      },
-    ],
-    []
-  );
-
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: "Column 1",
-        accessor: "col1", // accessor is the "key" in the data
-      },
-      {
-        Header: "Column 2",
-        accessor: "col2",
-      },
-    ],
-    []
-  );
+  const [inputValue, setInputValue] = useState("");
 
   const tableInstance = useTable({ columns, data });
 
   const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
     tableInstance;
 
+  const onChangeInput = (event) => {
+    setInputValue(event.target.value);
+  };
+
   return (
     <Styles>
+      <StyledInput value={inputValue} onChange={onChangeInput} />
       <table {...getTableProps()}>
         <TableHead headerGroups={headerGroups} />
         <TableBody
---
+++ client/src/pages/TableOrigin/ReactTableLib/columns.js
@@ -0,0 +1,10 @@
+export const columns = [
+  {
+    Header: "Column 1",
+    accessor: "col1", // accessor is the "key" in the data
+  },
+  {
+    Header: "Column 2",
+    accessor: "col2",
+  },
+];
